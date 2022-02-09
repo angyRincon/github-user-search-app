37288a87333e54de16f99d4fcde65b0d8adaad93
angyRincon <grincon@celerik.com>
2022-02-09 17:21:28 -0500

    Added hyperlink redirection

--- src/Components/Card/Details.js
+++ src/Components/Card/Details.js
@@ -2,27 +2,31 @@ const Details = ({location, twitter_username, blog, company}) => (
     <div className='details'>
         <div className='details-box'>
             <i className="fa-solid fa-location-dot"/>
-            <span>{ location ? location : 'Not Available' }</span>
+            <span>{location ? location : 'Not Available'}</span>
         </div>
 
         <div className='details-box'>
             <i className="fa-brands fa-twitter"/>
             <span>
-                { twitter_username ? twitter_username : 'Not Available' }
+                {twitter_username ? twitter_username : 'Not Available'}
             </span>
         </div>
 
         <div className='details-box'>
             <i className="fa-solid fa-paperclip"/>
             <span>
-                { blog ? blog : 'Not Available' }
+                {blog ?
+                    <a href={blog} target='_blank'>{blog}</a>
+                    :
+                    'Not Available'
+                }
             </span>
         </div>
 
         <div className='details-box'>
             <i className="fa-solid fa-building"/>
             <span>
-                { company ? company : 'Not Available' }
+                {company ? company : 'Not Available'}
             </span>
         </div>
     </div>
